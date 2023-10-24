91df6299d906a8599f524df2a8543b283fa25cd8
chaitanya709 <chaitanyaprasadam345@gamil.com>
2023-10-24 10:33:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Task4 } from "./Task4";
+function App() {
+  return (
+    <div className="App">  
+    <Task4/>
+    </div>
+  );
+  
+}
+// App.js is also a func comp?
+export default App;
